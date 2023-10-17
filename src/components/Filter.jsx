b0f8a91dbb4d3ab8551becfb4edd4996fcaeddf7
Steven Seley Jr <seleysteven2@gmail.com>
2023-10-17 13:41:24 -0500

    added desired filter buttons, bug: when you open one, you open all

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -43,6 +43,62 @@ const Filter = () => {
   
   return (
     <div className="container">
+      <div className="flex flex-col gap-2">
+        <motion.nav
+          initial={false}
+          animate={isOpen ? "open" : "closed"}
+          className="menu"
+        >
+          <motion.button
+            whileTap={{ scale: 0.97 }}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Color
+            <motion.div variants={menuVariants} style={{ originY: 0.55 }}>
+              <svg width="15" height="15" viewBox="0 0 20 20">
+                <path d="M0 7 L 20 7 L 10 16" />
+              </svg>
+            </motion.div>
+          </motion.button>
+          <motion.ul
+            variants={listVariants}
+            style={{ pointerEvents: isOpen ? "auto" : "none" }}
+          >
+            <motion.li variants={itemVariants}>Red </motion.li>
+            <motion.li variants={itemVariants}>Orange </motion.li>
+            <motion.li variants={itemVariants}> Green </motion.li>
+            <motion.li variants={itemVariants}>Black </motion.li>
+            <motion.li variants={itemVariants}> Grey </motion.li>
+          </motion.ul>
+        </motion.nav>
+        <motion.nav
+          initial={false}
+          animate={isOpen ? "open" : "closed"}
+          className="menu"
+        >
+          <motion.button
+            whileTap={{ scale: 0.97 }}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            Size
+            <motion.div variants={menuVariants} style={{ originY: 0.55 }}>
+              <svg width="15" height="15" viewBox="0 0 20 20">
+                <path d="M0 7 L 20 7 L 10 16" />
+              </svg>
+            </motion.div>
+          </motion.button>
+          <motion.ul
+            variants={listVariants}
+            style={{ pointerEvents: isOpen ? "auto" : "none" }}
+          >
+            <motion.li variants={itemVariants}>Small </motion.li>
+            <motion.li variants={itemVariants}>medium </motion.li>
+            <motion.li variants={itemVariants}> large </motion.li>
+            <motion.li variants={itemVariants}>X-Large</motion.li>
+            <motion.li variants={itemVariants}> 2X-Large</motion.li>
+          </motion.ul>
+        </motion.nav>
+      </div>
       <motion.nav
         initial={false}
         animate={isOpen ? "open" : "closed"}
@@ -52,7 +108,7 @@ const Filter = () => {
           whileTap={{ scale: 0.97 }}
           onClick={() => setIsOpen(!isOpen)}
         >
-          Color
+          Sort
           <motion.div variants={menuVariants} style={{ originY: 0.55 }}>
             <svg width="15" height="15" viewBox="0 0 20 20">
               <path d="M0 7 L 20 7 L 10 16" />
@@ -63,14 +119,10 @@ const Filter = () => {
           variants={listVariants}
           style={{ pointerEvents: isOpen ? "auto" : "none" }}
         >
-          <motion.li variants={itemVariants}>Red </motion.li>
-          <motion.li variants={itemVariants}>Orange </motion.li>
-          <motion.li variants={itemVariants}> Green </motion.li>
-          <motion.li variants={itemVariants}>Black </motion.li>
-          <motion.li variants={itemVariants}> Grey </motion.li>
+          <motion.li variants={itemVariants}>Price: </motion.li>
+          <motion.li variants={itemVariants}>Price: </motion.li>
         </motion.ul>
       </motion.nav>
-      <div className="."></div>
     </div>
   );
 };
