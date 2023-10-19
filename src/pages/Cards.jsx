90e2aab3b81d0e03818c49111c37b3f34f079e76
Steven Seley Jr <seleysteven2@gmail.com>
2023-10-19 01:31:35 -0500

    cleaned up code. need to divide up cards

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -37,15 +37,15 @@ const Cards = () => {
   const menuVariants = {
     open: { rotate: 180, transition: { duration: 0.2 } },
     closed: { rotate: 0, transition: { duration: 0.2 } },
-};
+  };
   
 const itemVariants = {
-    open: {
-        opacity: 1,
-        y: 0,
-        transition: { type: "spring", stiffness: 300, damping: 24 },
-    },
-    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
 };
   
 const listVariants = {
