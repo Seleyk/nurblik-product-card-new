b17dad853ed75359dd464e130f5ffa5caacb60d8
Steven Seley Jr <seleysteven2@gmail.com>
2023-11-18 17:16:00 -0600

    finnalized carousel

--- src/pages/Slider.jsx
+++ src/pages/Slider.jsx
@@ -24,13 +24,16 @@ const Slider = () => {
       <div className="relative w-full flex items-center max-w-[1500px]">
         <div className="absolute left-2 flex right-2 justify-between z-20">
           <button onClick={handleBack}>
-            <img src="/assets/leftArrow.svg" height={48} width={56} alt="" />
+            <svg className="text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path>
+            </svg>
           </button>
           <button onClick={handleNext}>
             <img src="/assets/rightArrow.svg" height={48} width={56} alt="" />
           </button>
         </div>
-        <motion.div className="flex gap-4 flex-nowrap"
+        <motion.div
+          className="flex gap-4 flex-nowrap"
           animate={{ x: `calc(-${currentIndex * 100}% - ${currentIndex}rem)` }}
           transition={transition}
         >
