3063931d16d8815e55fdb74783e951baa25a3fb0
Steven Seley Jr <seleysteven2@gmail.com>
2024-01-06 22:09:35 -0600

    cleaned carousel

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -17,6 +17,9 @@ const Carousel = () => {
     // TO DO: ADD PAGINATION
 
     <div className="mt-40 ">
+      <div className="text-black text-[32px] font-semibold mb-4 ml-40 tracking-[3.20px]">
+        FEATURED ITEMS
+      </div>
       <motion.div
         ref={carousel}
         className="carousel ml-40 cursor-grab overflow-hidden"
@@ -31,20 +34,20 @@ const Carousel = () => {
               key={item.id}
               layout
               className="flex flex-col gap-1)"
-              exit={{ x: -450, position: 'absolute' }}
+              exit={{ x: -450, position: "absolute" }}
               whileInView={{ y: [50, 0], opacity: [0, 1] }}
               transition={{ delay: 0.06, ...transition }}
             >
               {/* <Link to={"/details"}> */}
-                <div className="h-[380px] w-[350px] overflow-hidden rounded-xl cursor-pointer">
-                  <motion.img
-                    whileHover={{ scale: 1.2 }}
-                    className="w-full h-30vh object-cover rounded-xl"
-                    src={item.img}
-                    alt=" T-shirt"
-                    transition={transition}
-                  />
-                </div>
+              <div className="h-[380px] w-[350px] overflow-hidden rounded-xl cursor-pointer">
+                <motion.img
+                  whileHover={{ scale: 1.2 }}
+                  className="w-full h-30vh object-cover rounded-xl"
+                  src={item.img}
+                  alt=" T-shirt"
+                  transition={transition}
+                />
+              </div>
               {/* </Link>  */}
 
               <motion.span
@@ -59,6 +62,30 @@ const Carousel = () => {
           ))}
         </motion.div>
       </motion.div>
+      <div className="width-full flex justify-end mr-10 mb-16">
+        <button className="h-9 justify-center items-center inline-flex">
+          <div className="px-4 py-2 rounded justify-start items-center gap-1.5 flex">
+            <div className="justify-center items-center gap-2.5 flex">
+              <div className="text-black text-sm font-medium uppercase">
+                SHOP NOW
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="12"
+                viewBox="0 0 14 12"
+                fill="none"
+              >
+                <path
+                  d="M7.793 0.799012C7.98053 0.611541 8.23484 0.506226 8.5 0.506226C8.76516 0.506226 9.01947 0.611541 9.207 0.799012L13.707 5.29901C13.8945 5.48654 13.9998 5.74085 13.9998 6.00601C13.9998 6.27118 13.8945 6.52548 13.707 6.71301L9.207 11.213C9.0184 11.3952 8.7658 11.496 8.5036 11.4937C8.2414 11.4914 7.99059 11.3862 7.80518 11.2008C7.61977 11.0154 7.5146 10.7646 7.51233 10.5024C7.51005 10.2402 7.61084 9.98761 7.793 9.79901L10.5 7.00601H1C0.734784 7.00601 0.48043 6.90065 0.292893 6.71312C0.105357 6.52558 0 6.27123 0 6.00601C0 5.7408 0.105357 5.48644 0.292893 5.29891C0.48043 5.11137 0.734784 5.00601 1 5.00601H10.5L7.793 2.21301C7.60553 2.02548 7.50021 1.77118 7.50021 1.50601C7.50021 1.24085 7.60553 0.98654 7.793 0.799012Z"
+                  fill="black"
+                />
+              </svg>
+            </div>
+            <div className="py-0.5 flex-col justify-center items-center gap-2.5 inline-flex" />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Carousel from './Carousel'
+import Slideshow from './Slideshow'
+import SliderNew from './SliderNew'
 
 const Home = () => {
   return (
     <div>
+      <Slideshow />
+      <Carousel />
+      <SliderNew />
     </div>
   )
 }
