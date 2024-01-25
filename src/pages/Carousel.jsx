f8435c01ace10555de169ed17ab754828c2d169c
Steven Seley Jr <seleysteven2@gmail.com>
2024-01-25 17:40:59 -0600

    added aninmated filled cta

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -11,10 +11,9 @@ const Carousel = () => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
   }, []);
 
-
   const handleScroll = (direction) => {
     const { current } = carousel;
-    const scrollAmount = window.innerHeight > 1800 ? 270*1.75 : 210*1.75;
+    const scrollAmount = window.innerHeight > 1800 ? 270 * 1.75 : 210 * 1.75;
 
     if (direction === "left") {
       current.scrollLeft -= scrollAmount;
@@ -34,10 +33,16 @@ const Carousel = () => {
       </div>
       <div className="flex items-center justify-center">
         <div className="flex gap-2 mx-10 h-full flex-col items-center justify-center">
-          <motion.div onClick={() => handleScroll("right")} className=" cursor-pointer flex items-center justify-center max-md:w-10 w-20">
+          <motion.div
+            onClick={() => handleScroll("right")}
+            className=" cursor-pointer flex items-center justify-center max-md:w-10 w-20"
+          >
             <motion.img src="assets/rightArrow.svg" alt="" />
           </motion.div>
-          <motion.div onClick={() => handleScroll("left")} className=" cursor-pointer flex items-center justify-center max-md:w-10 w-20">
+          <motion.div
+            onClick={() => handleScroll("left")}
+            className=" cursor-pointer flex items-center justify-center max-md:w-10 w-20"
+          >
             <motion.img src="assets/leftArrow.svg" alt="" />
           </motion.div>
         </div>
@@ -82,28 +87,24 @@ const Carousel = () => {
           </motion.div>
         </motion.div>
       </div>
-      <div className="width-full flex justify-end mr-10 pb-16">
-        <button className="h-9 justify-center items-center inline-flex">
-          <div className="px-4 py-2 rounded justify-start items-center gap-1.5 flex">
-            <div className="justify-center items-center gap-2.5 flex">
-              <div className="text-black text-sm font-medium uppercase">
-                SHOP NOW
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="12"
-                viewBox="0 0 14 12"
-                fill="none"
-              >
-                <path
-                  d="M7.793 0.799012C7.98053 0.611541 8.23484 0.506226 8.5 0.506226C8.76516 0.506226 9.01947 0.611541 9.207 0.799012L13.707 5.29901C13.8945 5.48654 13.9998 5.74085 13.9998 6.00601C13.9998 6.27118 13.8945 6.52548 13.707 6.71301L9.207 11.213C9.0184 11.3952 8.7658 11.496 8.5036 11.4937C8.2414 11.4914 7.99059 11.3862 7.80518 11.2008C7.61977 11.0154 7.5146 10.7646 7.51233 10.5024C7.51005 10.2402 7.61084 9.98761 7.793 9.79901L10.5 7.00601H1C0.734784 7.00601 0.48043 6.90065 0.292893 6.71312C0.105357 6.52558 0 6.27123 0 6.00601C0 5.7408 0.105357 5.48644 0.292893 5.29891C0.48043 5.11137 0.734784 5.00601 1 5.00601H10.5L7.793 2.21301C7.60553 2.02548 7.50021 1.77118 7.50021 1.50601C7.50021 1.24085 7.60553 0.98654 7.793 0.799012Z"
-                  fill="black"
-                />
-              </svg>
-            </div>
-            <div className="py-0.5 flex-col justify-center items-center gap-2.5 inline-flex" />
-          </div>
+      <div className="width-full flex justify-end mr-10 pb-16 items-center">
+        <button class="btn-1 max-w-[180px]">
+          <span className="flex items-center justify-center gap-2.5">
+            <div className="">SHOP NOW </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="12"
+              viewBox="0 0 14 12"
+              fill="none"
+              className="arrow"
+            >
+              <path
+                d="M7.793 0.799012C7.98053 0.611541 8.23484 0.506226 8.5 0.506226C8.76516 0.506226 9.01947 0.611541 9.207 0.799012L13.707 5.29901C13.8945 5.48654 13.9998 5.74085 13.9998 6.00601C13.9998 6.27118 13.8945 6.52548 13.707 6.71301L9.207 11.213C9.0184 11.3952 8.7658 11.496 8.5036 11.4937C8.2414 11.4914 7.99059 11.3862 7.80518 11.2008C7.61977 11.0154 7.5146 10.7646 7.51233 10.5024C7.51005 10.2402 7.61084 9.98761 7.793 9.79901L10.5 7.00601H1C0.734784 7.00601 0.48043 6.90065 0.292893 6.71312C0.105357 6.52558 0 6.27123 0 6.00601C0 5.7408 0.105357 5.48644 0.292893 5.29891C0.48043 5.11137 0.734784 5.00601 1 5.00601H10.5L7.793 2.21301C7.60553 2.02548 7.50021 1.77118 7.50021 1.50601C7.50021 1.24085 7.60553 0.98654 7.793 0.799012Z"
+                fill="black"
+              />
+            </svg>{" "}
+          </span>
         </button>
       </div>
     </div>
