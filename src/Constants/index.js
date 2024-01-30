f85737ee8706dd836b649b2ae8b14ee6f272efb2
Steven Seley Jr <seleysteven2@gmail.com>
2024-01-30 15:26:22 -0600

    added navbar, constants, and assets

---
+++ src/Constants/index.js
@@ -0,0 +1,26 @@
+// api endpoints
+export const AUTH_URL = `/api/auth`;
+
+// Frontend navigation links
+export const navLinks = [
+  {
+    label: "New Arrivals",
+    route: "/collections/new-arrivals",
+  },
+  {
+    label: "Men",
+    route: "/collections/men",
+  },
+  {
+    label: "Women",
+    route: "/collections/women",
+  },
+  {
+    label: "Collections",
+    route: "/collections",
+  },
+  {
+    label: "About",
+    route: "/About",
+  },
+];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "@mui/material";
+import { navLinks } from "../Constants";
 
 const Navbar = () => {
-  return (
-    <div className='absolute top-5 right-8 z-50 flex gap-3'>
-      <div className="cursor-pointer hover:underline"> Cart </div>
-      <div className="cursor-pointer hover:underline"> Menu </div>
-    </div>
-  )
-}
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
 
-export default Navbar
+  return (
+    <div className="w-full absolute z-50 h-20 px-8 justify-between items-center inline-flex">
+      <NavLink
+        to="/"
+        className="text-secondary-700 max-lg:text-[24px] max-lg:tracking-[2.20px] text-[32px] font-bold font-['Roboto'] tracking-[3.20px]"
+      >
+        NURBLIK KOUTURE
+      </NavLink>
+      {isNonMobileScreen && (
+        <div className="justify-center items-center gap-9 flex">
+          {navLinks.map((link) => (
+            <NavLink
+              to={link.route}
+              key={link.label}
+              className="text-secondary-700 uppercase text-xs font-medium font-['Roboto'] tracking-wide"
+            >
+              {link.label}
+            </NavLink>
+          ))}
+        </div>
+      )}
+      <div className="justify-center items-center gap-8 flex max-lg:pl-8 pl-52">
+        {isNonMobileScreen && (
+          <NavLink to={`/profile`}>
+            <img
+              src="/assets/user.svg"
+              alt="notifications"
+              width={24}
+              height={24}
+            />
+          </NavLink>
+        )}
+        <NavLink to={`/cart`}>
+        <img
+          src="/assets/cart.svg"
+          alt="notifications"
+          width={24}
+          height={24}
+        />
+        </NavLink>
+        {!isNonMobileScreen ? (
+          <img
+            src="/assets/menu.svg"
+            alt="notifications"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
