5bb049b1d1d39c87c3375bb212b0bc93f1c30cc5
Steven Seley Jr <seleysteven2@gmail.com>
2024-01-30 15:06:02 -0600

    stopping point. added link to featured items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='absolute top-5 right-8 cursor-pointer hover:underline z-50'>Menu</div>
+    <div className='absolute top-5 right-8 z-50 flex gap-3'>
+      <div className="cursor-pointer hover:underline"> Cart </div>
+      <div className="cursor-pointer hover:underline"> Menu </div>
+    </div>
   )
 }
 
--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -63,16 +63,16 @@ const Carousel = () => {
                 transition={{ delay: 0.05, ...transition }}
               >
                 <Link to={"/details"}>
-                <div className="h-[380px] w-[350px] overflow-hidden rounded-xl cursor-pointer">
-                  <motion.img
-                    whileHover={{ scale: 1.2 }}
-                    className="w-full h-30vh object-cover rounded-xl"
-                    src={item.img}
-                    alt=" T-shirt"
-                    transition={transition}
-                  />
-                </div>
-                </Link> 
+                  <div className="h-[380px] w-[350px] overflow-hidden rounded-xl cursor-pointer">
+                    <motion.img
+                      whileHover={{ scale: 1.2 }}
+                      className="w-full h-30vh object-cover rounded-xl"
+                      src={item.img}
+                      alt=" T-shirt"
+                      transition={transition}
+                    />
+                  </div>
+                </Link>
 
                 <motion.span
                   exit={{ opacity: 0 }}
@@ -88,24 +88,26 @@ const Carousel = () => {
         </motion.div>
       </div>
       <div className="width-full flex justify-end mr-10 pb-16 items-center">
-        <button class="btn-1 max-w-[180px]">
-          <span className="flex items-center justify-center gap-2.5">
-            <div className="">SHOP NOW </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="12"
-              viewBox="0 0 14 12"
-              fill="none"
-              className="arrow"
-            >
-              <path
-                d="M7.793 0.799012C7.98053 0.611541 8.23484 0.506226 8.5 0.506226C8.76516 0.506226 9.01947 0.611541 9.207 0.799012L13.707 5.29901C13.8945 5.48654 13.9998 5.74085 13.9998 6.00601C13.9998 6.27118 13.8945 6.52548 13.707 6.71301L9.207 11.213C9.0184 11.3952 8.7658 11.496 8.5036 11.4937C8.2414 11.4914 7.99059 11.3862 7.80518 11.2008C7.61977 11.0154 7.5146 10.7646 7.51233 10.5024C7.51005 10.2402 7.61084 9.98761 7.793 9.79901L10.5 7.00601H1C0.734784 7.00601 0.48043 6.90065 0.292893 6.71312C0.105357 6.52558 0 6.27123 0 6.00601C0 5.7408 0.105357 5.48644 0.292893 5.29891C0.48043 5.11137 0.734784 5.00601 1 5.00601H10.5L7.793 2.21301C7.60553 2.02548 7.50021 1.77118 7.50021 1.50601C7.50021 1.24085 7.60553 0.98654 7.793 0.799012Z"
-                fill="black"
-              />
-            </svg>{" "}
-          </span>
-        </button>
+        <Link to={"/cards"}>
+          <button class="btn-1 max-w-[180px]">
+            <span className="flex items-center justify-center gap-2.5">
+              <div className="">SHOP NOW </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="12"
+                viewBox="0 0 14 12"
+                fill="none"
+                className="arrow"
+              >
+                <path
+                  d="M7.793 0.799012C7.98053 0.611541 8.23484 0.506226 8.5 0.506226C8.76516 0.506226 9.01947 0.611541 9.207 0.799012L13.707 5.29901C13.8945 5.48654 13.9998 5.74085 13.9998 6.00601C13.9998 6.27118 13.8945 6.52548 13.707 6.71301L9.207 11.213C9.0184 11.3952 8.7658 11.496 8.5036 11.4937C8.2414 11.4914 7.99059 11.3862 7.80518 11.2008C7.61977 11.0154 7.5146 10.7646 7.51233 10.5024C7.51005 10.2402 7.61084 9.98761 7.793 9.79901L10.5 7.00601H1C0.734784 7.00601 0.48043 6.90065 0.292893 6.71312C0.105357 6.52558 0 6.27123 0 6.00601C0 5.7408 0.105357 5.48644 0.292893 5.29891C0.48043 5.11137 0.734784 5.00601 1 5.00601H10.5L7.793 2.21301C7.60553 2.02548 7.50021 1.77118 7.50021 1.50601C7.50021 1.24085 7.60553 0.98654 7.793 0.799012Z"
+                  fill="black"
+                />
+              </svg>{" "}
+            </span>
+          </button>
+        </Link>
       </div>
     </div>
   );
