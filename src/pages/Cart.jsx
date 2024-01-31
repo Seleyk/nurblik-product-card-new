30d508282e747e83a9cc4a2af690000d34d7374c
Steven Seley Jr <seleysteven2@gmail.com>
2024-01-31 04:18:45 -0600

    stopping point added markup for cart details. have not finneished details for cart items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,15 @@ const Cart = () => {
     <div className="pt-24 px-6 flex gap-6 max-md:flex-col">
       <div className="w-[60%] max-md:w-full">
         <h1>Shopping cart</h1>
-        <div className="mt-8">items</div>
+        <div className="mt-8">
+          <div className="flex gap-4 mt-4 ">
+            <div className="h-[158px] w-[155px] rounded-xl bg-black"></div>
+            <div className="flex max-md:flex-col justify-between">
+              <div className="">list</div>
+              <div className="">rest of list</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-[40%] max-md:w-full">
         <h1 className="max-md:text-center">Summary</h1>
@@ -23,12 +31,15 @@ const Cart = () => {
             <span>yes</span>
           </div>
         </div>
-        <div className="flex flex-col gap-3 mt-20">
+        <div className="flex flex-col gap-3 mt-52">
           <div className="bg-black h-[1.5px] w-full"></div>
           <div className="flex justify-between">
             <span className="">answer</span>
             <span>yes</span>
           </div>
+          <button className="cta">
+            <span className="cta-txt">Checkout</span>
+          </button>
         </div>
       </div>
     </div>
