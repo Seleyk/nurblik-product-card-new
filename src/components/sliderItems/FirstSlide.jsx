497252b568a0cbd93de1312090692960166afadb
Steven Seley Jr <seleysteven2@gmail.com>
2024-02-07 23:58:02 -0600

    first slide responsive

--- src/components/sliderItems/FirstSlide.jsx
+++ src/components/sliderItems/FirstSlide.jsx
@@ -7,14 +7,14 @@ const FirstSlide = () => {
   return (
     <motion.div className="h-full relative bg-[#E3E2DF] overflow-hidden">
       <img
-        className="absolute bottom-0 right-[397px]"
+        className="absolute bottom-0 right-[397px] max-md:right-1 max-md:w-[393px]"
         src="/assets/photo-1532332248682-206cc786359f (1).png"
         alt=""
       />
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={transition}
-        className="w-[633px] left-40 bottom-48 h-[298px] absolute text-black text-[80px] font-bold tracking-[8px]"
+        className="w-[633px] left-40 max-md:left-7 bottom-48 max-md:bottom-80 h-[298px] absolute text-black text-[80px] max-md:text-5xl font-bold tracking-[8px]"
       >
         SHOP
         <br />
