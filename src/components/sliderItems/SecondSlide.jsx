c2d4bf60e1f1a817943571a13fefc783086e95e9
Steven Seley Jr <seleysteven2@gmail.com>
2024-02-08 01:00:06 -0600

    third slide

--- src/components/sliderItems/SecondSlide.jsx
+++ src/components/sliderItems/SecondSlide.jsx
@@ -6,9 +6,9 @@ const SecondSlide = () => {
 
   return (
     <motion.div className=" relative h-[110vh] bg-gray-500">
-      <div className="w-full absolute flex justify-center">
+      <div className="w-full absolute bottom-10 flex justify-center max-md:w-full">
         <img
-          className=" w-[566px] bottom-10 z-20"
+          className=" w-[566px] max-md:bottom-0 z-20"
           src="/assets/51a4dd4c4714e997f5c9e8402ed04e56.png"
           alt=""
         />
@@ -16,7 +16,7 @@ const SecondSlide = () => {
       <motion.div
         whileInView={{ y: [50, 0], opacity: [0, 1] }}
         transition={transition}
-        className="absolute w-[705px] text-center text-stone-100 text-[80px] font-bold tracking-[6.40px] right-1/4 bottom-[10%] z-40"
+        className="absolute w-[705px] max-md:w-full text-center text-stone-100 text-[80px] max-md:text-5xl max-md:text-black max-md:z-10 font-bold tracking-[6.40px] right-1/4 max-md:right-0 bottom-[10%] max-md:bottom-[550px] max-md: z-40"
       >
         THE TECHWEAR COLLECTION IS HERE
       </motion.div>
--- src/components/sliderItems/ThirdSlide.jsx
+++ src/components/sliderItems/ThirdSlide.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const ThirdSlide = () => {
-  return (
-    <div className="h-[110vh] bg-[#564A47]"></div>
-  )
-}
+  const transition = { duration: 2, ease: [0.43, 0.13, 0.23, 0.96] };
 
-export default ThirdSlide
+  return (
+    <motion.div className="h-full relative bg-[#564A47] overflow-hidden">
+      <motion.img
+        whileInView={{ opacity: [0, 1] }}
+        transition={transition}
+        className=" z-20 w-[596px] absolute bottom-0 right-[397px] max-md:right-1 max-md:w-[393px]"
+        src="/assets/Slide3img.png"
+        alt=""
+      />
+      <motion.div
+        whileInView={{ opacity: [0, 1], left: [0, 16] }}
+        transition={{delay: .75, ...transition}}
+        className="z-10 uppercase left-10 max-md:left-1 bottom-96 max-md:bottom-60 h-[298px] absolute text-black text-[80px] max-md:text-5xl font-bold tracking-[8px]"
+      >
+        Windbreakers
+        <br />
+        Jackets
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1], right: [0, 40] }}
+        transition={{delay: .75, ...transition}}
+        className="uppercase right-10 max-md:right-7 bottom-0 max-md:bottom-0 max-md:z-30 h-[298px] max-md:h-[240px] absolute text-black text-[80px] max-md:text-5xl font-bold tracking-[8px]"
+      >
+        50% off
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default ThirdSlide;
