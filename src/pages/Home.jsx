bd734acc6fc2ae354a655181f864d48558c092da
Steven Seley Jr <seleysteven2@gmail.com>
2024-02-22 15:14:41 -0600

    made new arrivals slideshow responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Carousel from "./Carousel";
 import Slideshow from "./Slideshow";
 import SliderNew from "./SliderNew";
+import { useMediaQuery } from "@mui/material";
 
 const Home = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+
   return (
     <div>
       <Slideshow />
       <Carousel />
-      <SliderNew />
+      {isNonMobileScreen && <SliderNew />}
     </div>
   );
 };
