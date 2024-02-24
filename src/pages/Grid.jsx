437d6c5ba34f0b0e2c9c96a03c59bfe85da147dc
Steven Seley Jr <seleysteven2@gmail.com>
2024-02-23 23:10:14 -0600

    added popular collections assets

--- src/pages/Grid.jsx
+++ src/pages/Grid.jsx
@@ -9,22 +9,53 @@ const Grid = () => {
       <div className="pt-20 self-center w-full flex items-center justify-center uppercase text-[32px] font-semibold tracking-[3.20px]">
         popular collections
       </div>
-      <div className="w-full h-[40rem] mt-9 grid grid-cols-3 gap-4">
+      <div className="w-full h-[45rem] mt-9 grid grid-cols-3 gap-4">
         <div className="col-span-2 grid grid-rows-2 gap-4 ">
           <div className="grid grid-cols-2 gap-4">
-            <div className="bg-black cursor-pointer"></div>
-            <div className="bg-black cursor-pointer"></div>
+            <div className="max-h-[350px] overflow-hidden bg-black cursor-pointer">
+              <img
+                src="https://i.pinimg.com/564x/ec/9c/48/ec9c48cf69432321608eb367ab9fa246.jpg"
+                alt=""
+                className="h-full w-full bg-cover bg-no-repeat"
+              />
+            </div>
+            <div className="max-h-[350px] overflow-hidden bg-black cursor-pointer">
+              <img
+                src="/assets/grid2.png"
+                alt=""
+                className="h-full w-full bg-cover bg-no-repeat"
+              />
+            </div>
           </div>
-          <div className="bg-black cursor-pointer"></div>
+          <div className="max-h-[350px] overflow-hidden bg-black cursor-pointer">
+              <img
+                src="/assets/grid5.jpg"
+                alt=""
+                className="h-full w-full bg-cover bg-no-repeat"
+              />
+            </div>
         </div>
-        <div className=" col-span-1 bg-black cursor-pointer"></div>
+        <div className="max-h-[720px] col-span-1 bg-black cursor-pointer">
+          <img
+            src="/assets/grid.jpg"
+            alt=""
+            className="h-full w-full bg-cover bg-no-repeat"
+          />
+        </div>
       </div>
     </div>
   ) : (
     <div className="w-full">
-      <div className="h-[103dvh] bg-slate-300"></div>
-      <div className="h-[103dvh] bg-slate-400"></div>
+      <div className="h-[103dvh] bg-slate-300">
+        <img
+          src="/assets/grid.jpg"
+          alt=""
+          className="h-full w-full bg-cover bg-no-repeat"
+        />
+      </div>
+      <div className="h-[103dvh]  bg-slate-400"></div>
       <div className="h-[103dvh] bg-slate-500"></div>
+      <div className="h-[103dvh] bg-slate-700"></div>
     </div>
   );
 };
