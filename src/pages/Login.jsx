8495f9ccf69772fb41b161168c5066419c6f0583
Steven Seley Jr <seleysteven2@gmail.com>
2024-02-27 17:50:26 -0600

    fixed login spacing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full relative">
+    <div className="w-full relative ">
       <video
         className="w-full h-full object-cover absolute -z-10"
         src="/assets/loginvid.mp4"
@@ -20,7 +20,7 @@ const Login = () => {
         muted
         loop
       />
-      <div className="h-full w-full px-32 max-md:w-full max-md:p-0 flex items-center justify-end ">
+      <div className=" md:py-20 h-full w-full px-32 max-md:w-full max-md:p-0 flex items-center justify-end ">
         <div className="bg-[#F8F7F4] bg-opacity-95 flex flex-col gap-6 w-[480px] p-8 max-md:h-full max-md:w-full max-md:rounded-[0] max-md:pt-20 rounded-3xl ">
           <div className="flex flex-col items-center gap-1 ">
             <h1 className="text-black text-[32px] font-semibold tracking-wider">
