dd6fc3c4e5b5be238ed0790a14aa3fe11b953a73
Steven Seley Jr <seleysteven2@gmail.com>
2024-03-08 11:21:05 -0600

    right side order confirmation finnished

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { items } from "../data";
 
 const Success = () => {
   return (
@@ -67,10 +68,158 @@ const Success = () => {
               </div>
             </div>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 md:overflow-y-auto">
             <div className="flex flex-col gap-4">
               <div className="text-2xl font-semibold tracking-wider uppercase">
                 order summary
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="h-[2px] bg-black w-full"></div>
+                <div className="flex flex-col gap-2">
+                  <div className="flex">
+                    <div className="flex-1 capitalize text-sm text-[#6f6f6f]">
+                      date
+                    </div>
+                    <div className="flex-1 capitalize text-sm text-[#6f6f6f]">
+                      order confirmation ID
+                    </div>
+                  </div>
+                  <div className="flex font-semibold">
+                    <div className="flex-1 capitalize text-sm">18 Feb 2024</div>
+                    <div className="flex-1 capitalize text-sm">
+                      65cdfd9951312c85ded44252
+                    </div>
+                  </div>
+                </div>
+                <div className="h-[2px] bg-black w-full"></div>
+              </div>
+              <div className="text-lg font-semibold capitalize">Products</div>
+              <div className="flex flex-1 flex-col md:pr-20">
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-4">
+                    <div className="min-w-[112px] h-32 bg-black rounded-xl"></div>
+                    <div className="flex flex-col gap-2">
+                      <div className="font-bold text-sm tracking-widest uppercase">
+                        “OFF-WHITE” X AIR-JORDAN 1
+                      </div>
+                      <div className="flex gap-2">
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">Size:{" "}</span>
+                          {`small`}
+                        </div>
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">color:{" "}</span>
+                          {`black`}
+                        </div>
+                      </div>
+                      <div className="font-normal text-xs capitalize">
+                        <span className="text-[#6f6f6f]">qty:{" "}</span>
+                        {1}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[2px] bg-black w-full"></div>
+                </div>
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-4">
+                    <div className="min-w-[112px] h-32 bg-black rounded-xl"></div>
+                    <div className="flex flex-col gap-2">
+                      <div className="font-bold text-sm tracking-widest uppercase">
+                        {items[0].title}
+                      </div>
+                      <div className="flex gap-2">
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">Size:{" "}</span>
+                          {`small`}
+                        </div>
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">color:{" "}</span>
+                          {`black`}
+                        </div>
+                      </div>
+                      <div className="font-normal text-xs capitalize">
+                        <span className="text-[#6f6f6f]">qty:{" "}</span>
+                        {3}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[2px] bg-black w-full"></div>
+                </div>
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-4">
+                    <div className="min-w-[112px] h-32 bg-black rounded-xl"></div>
+                    <div className="flex flex-col gap-2">
+                      <div className="font-bold text-sm tracking-widest uppercase">
+                      {items[2].title}
+                      </div>
+                      <div className="flex gap-2">
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">Size:{" "}</span>
+                          {`small`}
+                        </div>
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">color:{" "}</span>
+                          {`black`}
+                        </div>
+                      </div>
+                      <div className="font-normal text-xs capitalize">
+                        <span className="text-[#6f6f6f]">qty:{" "}</span>
+                        {1}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[2px] bg-black w-full"></div>
+                </div>
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-4">
+                    <div className="min-w-[112px] h-32 bg-black rounded-xl"></div>
+                    <div className="flex flex-col gap-2">
+                      <div className="font-bold text-sm tracking-widest uppercase">
+                      {items[4].title}
+                      </div>
+                      <div className="flex gap-2">
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">Size:{" "}</span>
+                          {`small`}
+                        </div>
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">color:{" "}</span>
+                          {`black`}
+                        </div>
+                      </div>
+                      <div className="font-normal text-xs capitalize">
+                        <span className="text-[#6f6f6f]">qty:{" "}</span>
+                        {1}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[2px] bg-black w-full"></div>
+                </div>
+                <div className="flex flex-col gap-2 mt-2">
+                  <div className="flex gap-4">
+                    <div className="min-w-[112px] h-32 bg-black rounded-xl"></div>
+                    <div className="flex flex-col gap-2">
+                      <div className="font-bold text-sm tracking-widest uppercase">
+                      {items[6].title}
+                      </div>
+                      <div className="flex gap-2">
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">Size:{" "}</span>
+                          {`small`}
+                        </div>
+                        <div className="font-normal text-xs capitalize">
+                          <span className="text-[#6f6f6f]">color:{" "}</span>
+                          {`black`}
+                        </div>
+                      </div>
+                      <div className="font-normal text-xs capitalize">
+                        <span className="text-[#6f6f6f]">qty:{" "}</span>
+                        {1}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="h-[2px] bg-black w-full"></div>
+                </div>
               </div>
             </div>
           </div>
